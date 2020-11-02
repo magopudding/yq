3b8a017839ac9891070810f714449cb30369353e
xiangyun.kong <kongxiangyun@yxqiche.com>
2020-11-02 16:42:41 +0800

    两数相加

--- yq-algorithm/src/main/java/TwoNoSum.java
+++ yq-algorithm/src/main/java/TwoNoSum.java
@@ -1,30 +1,24 @@
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
  * @ClassName: TwoNoSum
- * @Descraption: 给定一个整数数组 nums 和一个目标值 target，请你在该数组中找出和为目标值的那 两个 整数，并返回他们的数组下标。
- *
- * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素不能使用两遍。
- * 示例:
- * 给定 nums = [2, 7, 11, 15], target = 9
-
- * 因为 nums[0] + nums[1] = 2 + 7 = 9
- * 所以返回 [0, 1]
- *
- * 来源：力扣（LeetCode）
- * 链接：https://leetcode-cn.com/problems/two-sum
+ * @Descraption:
  **/
 public class TwoNoSum {
 
   /*
-   *
-   * @Description //通过map寻找 target-x
+   * @Description //两数之和  leetcode no1
    * @Date  2020/11/2 14:28
    * @Param [nums, target]
    * @return int[]
    **/
   public int[] twoSum(int[] nums, int target) {
+      if(nums == null || nums.length == 0){
+        return new int[]{};
+      }
       Map<Integer,Integer> numMap = new HashMap<>();
       int length = nums.length;
       for(int i = 0; i< length; i++){
@@ -34,5 +28,69 @@ public class TwoNoSum {
          numMap.put(nums[i],i);
       }
       return new int[]{};
+  }
+
+  /*
+   *
+   * @Description //两数相加 leetcode no2
+   * @Date  2020/11/2 16:41
+   * @Param [l1, l2]
+   * @return TwoNoSum.ListNode
+   **/
+  public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+      ListNode result = new ListNode(0); //结果
+      ListNode current = result; //类似于最终结果集的指针，填充完一个结果值之后，就指向下一个节点
+      int temp = 0;//如果两个数相加的结果大于9，就将其十位记录下来，给下一位做进位用
+      while (l1 != null || l2 != null){//当l1和l2都不等于空时执行
+          int val1 = 0;
+          if(l1 != null){
+          val1 = l1.val;
+          l1 = l1.next;
+      }
+      int val2 = 0;
+      if(l2 != null){
+          val2 = l2.val;
+          l2 = l2.next;
+      }
+      int val3;
+
+      val3 = val1 + val2 + temp;
+      temp = 0;//将进位值加过之后重置
+      if(val3 / 10 != 0){ //如果相加结果大于0
+        temp = val3 / 10;//需要进位的数给temp
+        val3 = val3 % 10;//个位留下
+      }
+      current.next = new ListNode(val3);
+      current = current.next;
+    }
+    if(temp > 0){
+      current.next = new ListNode(temp);
+    }
+    return result.next;
+    }
+
+  public static void main(String[] args) {
+    TwoNoSum t = new TwoNoSum();
+
+    ListNode l1 = t.new ListNode(3);
+    ListNode l2 = t.new ListNode(4);
+    ListNode l3 = t.new ListNode(2);
+    l1.next = l2;
+    l2.next = l3;
+
+    ListNode l4 = t.new ListNode(9);
+    ListNode l5 = t.new ListNode(6);
+    ListNode l6 = t.new ListNode(9);
+    l4.next = l5;
+    l5.next = l6;
+    ListNode listNode = t.addTwoNumbers(l1, l4);
+  }
+  public class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
   }
 }
