0591d5cd48b5790b8eabe7dc8b2dc639eaa22db1
xiangyun.kong <kongxiangyun@yxqiche.com>
2020-11-20 17:30:25 +0800

    'yd0'

---
+++ yq-algorithm/src/main/java/MoveZeroes.java
@@ -0,0 +1,22 @@
+/**
+ * @ClassName: MoveZeroes
+ * @Descraption: TODO
+ * @Author: kongxiangyun
+ * @Date: 2020/11/19 17:08
+ * @Version 1.0
+ **/
+public class MoveZeroes {
+
+  /*
+   *
+   * @Description leetcode no 283 移动零
+   * @Date  2020/11/19 17:08
+   * @Param [nums]
+   * @return void
+   **/
+  public void moveZeroes(int[] nums) {
+
+  }
+
+
+}
